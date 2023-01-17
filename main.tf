9a22f792581c5016154ea8403b7ec107a7028850
Trustedsavvy <118703403+Trustedsavvy@users.noreply.github.com>
2023-01-17 08:33:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,12 @@
-data "azurerm_resource_group" "example" {
-  name     = "pipelinerg"
-    }
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
 resource "azurerm_public_ip" "example" {
-  name                = "PIp0100"
-  resource_group_name = data.azurerm_resource_group.example.name
-  location            = data.azurerm_resource_group.example.location
+  name                = "acceptanceTestPublicIp1"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
   allocation_method   = "Static"
 
   tags = {
